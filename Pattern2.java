f297bc7a037cf65bf8a4a25e0450220182fd9eba
qnkarthika <134907181+qnkarthika@users.noreply.github.com>
2024-06-10 19:03:26 +0530

    Create Pattern2.java

---
+++ Pattern2.java
@@ -0,0 +1,23 @@
+Input : N=5
+Output:
+*
+* *
+* * *
+* * * *
+* * * * *
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        
+        int n=s.nextInt();
+        
+        for(int i=0;i<n;i++){
+             for(int j=0;j<=i;j++){
+                System.out.print("* ");
+             }
+                System.out.println();
+        }
+    }
+}
