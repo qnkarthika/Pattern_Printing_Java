4d82404bbeb6ca9ec6feb5948fc7b1f436acef2c
qnkarthika <134907181+qnkarthika@users.noreply.github.com>
2024-06-11 11:41:02 +0530

    Create Pattern9.java

---
+++ Pattern9.java
@@ -0,0 +1,31 @@
+Input: N=5
+Output:
+1 3 6 10 15
+2 5 9 14
+4 8 13
+7 12
+11
+  
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+     Scanner s=new Scanner(System.in);
+     int n=s.nextInt();
+     int start=1;
+     int step=2;
+     int x,y;
+     for(int i=1;i<=n;i++){
+           x=start;
+           y=step;
+           for(int j=0;j<=n-i;j++){
+               System.out.print(x+" ");
+               x+=y;
+               y++;
+           }
+       System.out.println();
+       start+=i;
+       step++;
+    }
+
+  }
+}
